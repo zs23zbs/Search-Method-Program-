328b82f3b9a37eb69bfe767d69a547712417e7d5
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-08 08:41:20 -0500

    Created search tree for algorithms to search through

--- searchAlgo.py
+++ searchAlgo.py
@@ -1,6 +1,9 @@
 from collections import deque 
 
-search_tree = { 
+search_tree = { # Based on Adjacencies.txt input file, looks as so becuase it is bidirectional (symmetrical)
     "Anthony" : ["Bluff_City", "Argonia", "Harper"],
-    "Bluff_City" : []
+    "Bluff_City" : ["Anthony", "Kiowa", "South_Haven", "Mayfield"],
+    "Kiowa" : ["Bluff_City", "Attica", "Coldwater"],
+    "Harper" : ["Anthony", "Attica"],
+    "Argonia" : ["Anthony", "Rago", "Caldwell"]
 }
